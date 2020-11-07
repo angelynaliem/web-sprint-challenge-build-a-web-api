d9ba05279dd01e7cde73a197a02177aa4111ad25
angelynaliem <angelynaliem@gmail.com>
2020-11-06 22:09:10 -0500

    Added server.js and router files

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+require("dotenv").config()
+
+const server = require("./server.js")
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+})
+
