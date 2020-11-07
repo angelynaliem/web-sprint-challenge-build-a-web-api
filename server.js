ce9e9e0289db3f6ee355c07de86acdba45da73de
angelynaliem <angelynaliem@gmail.com>
2020-11-06 22:12:12 -0500

    Added .env

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const cors = require("cors")
 
 const server = express()
 
-const projectRouter = require("./routers/projectRouter.js")
-const actionRouter = require("./routers/actionRouter.js")
+// const projectRouter = require("./routers/projectRouter.js")
+// const actionRouter = require("./routers/actionRouter.js")
 
 server.use(express.json())
 server.use(cors())
 
-server.use("/api/project", projectRouter)
-server.use("/api/action", actionRouter)
+// server.use("/api/project", projectRouter)
+// server.use("/api/action", actionRouter)
 
 server.get("/", (req, res) => {
     res.send(`<h1>Let's make this happen!</h1>`)
